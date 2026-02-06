b0942aa059596ac30f5c6f7f606ae0e2f119147f
David <grantclan.7@gmail.com>
2026-02-06 18:15:32 +0000

    seeded data with properties agents etc

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,8 +1,8 @@
 import type { SanityImageSource } from "@sanity/image-url";
-import { createImageUrlBuilder } from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./client";
 
-const builder = createImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source);
