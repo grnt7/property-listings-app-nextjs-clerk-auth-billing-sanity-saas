d780e1e90f1ea87ba5261a836b7872b7f3c6231d
David <grantclan.7@gmail.com>
2026-02-06 20:15:31 +0000

    modified for deployment

--- lib/sanity/image.ts
+++ lib/sanity/image.ts
@@ -1,8 +1,7 @@
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { createImageUrlBuilder, type SanityImageSource } from "@sanity/image-url";
 import { client } from "./client";
 
-const builder = imageUrlBuilder(client);
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source);
